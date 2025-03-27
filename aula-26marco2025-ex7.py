6971ccc18bd2b5993a338ac20205289890a472a9
Jowilson-JowJow <captares@gmail.com>
2025-03-26 21:53:19 -0400

    exercícios
    feitos em sala de aula para fixação dos comando de lista

---
+++ aula-26marco2025-ex7.py
@@ -0,0 +1,4 @@
+#voce precisa adicionar o elemento "Brasil" nos dados abaixo: country = ("Alemanha", "Italia", "Japão")
+country=["Alemanha", "Italia", "Japão"]
+country[1:1]=["Brasil"]
+print(country)
---
+++ aula-26marco2025-ex8.py
@@ -0,0 +1,8 @@
+#voce precisa adicionar o elemento "Brasil" nos dados abaixo: country = ("Alemanha", "Italia", "Japão")
+#adicionar todos os paises da america do sul
+#adicionar todos os paises da america do norte
+#adicionar todos os paises da america central
+#adicionar as capitais de todos os paises adicionados
+country=["Alemanha", "Italia", "Japão"]
+country[2:2]=["Argentina", "Buenos Aires", "Bolívia", "Sucre/La Paz", "Brasil", "Brasília", "Chile", "Santiago", "Colômbia", "Bogotá", "Equador", "Quito", "Guiana", "Georgetown", "Paraguai", "Assunção", "Peru", "Lima", "Suriname", "Paramaribo", "Uruguai", "Montevidéu", "Venezuela", "Caracas", "Canadá", "Ottawa", "Estados Unidos", "Washington, D.C.", "México", "Cidade do México", "Antígua e Barbuda", "Saint John's", "Bahamas", "Nassau", "Barbados", "Bridgetown", "Belize", "Belmopan", "Costa Rica", "San José", "Cuba", "Havana", "El Salvador", "San Salvador", "Granada", "St. George's", "Guatemala", "Cidade da Guatemala", "Honduras", "Tegucigalpa", "Jamaica", "Kingston", "Nicarágua", "Manágua", "Panamá", "Cidade do Panamá", "República Dominicana", "Santo Domingo", "São Cristóvão e Nevis", "Basseterre", "Santa Lúcia", "Castries", "São Vicente e Granadinas", "Kingstown", "Trindade e Tobago", "Porto de Espanha"]
+print(country)
