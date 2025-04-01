c9738fc697d963c64f17232479dff01de3437c3d
Jowilson-JowJow <captares@gmail.com>
2025-03-31 21:55:34 -0400

    python com thiago
    thiago substituiu o edersom em 31/03/2025

---
+++ aula-py-thiago-31-03-25.py
@@ -0,0 +1,7 @@
+# fazer um codigo --- se for quarta assisitir o jogo do timão senão estudar
+dia=input("Digite o dia da semana:")
+if dia=="quarta" or dia=="quarta feira" or dia=="quarta-feira":
+    print("Assistir o jogo do TIMÂO")
+else:
+    print("Estudar")
+
---
+++ aula-py-thiago1-31-03-25.py
@@ -0,0 +1,7 @@
+#upper retorna maiuscula
+#lower retorna minusculo
+
+letra=input("Digite uma letra:").upper()
+letra1=input("Digite uma letra:").lower()
+print(letra)
+print(letra1)
