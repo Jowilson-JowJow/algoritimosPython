4f40150b2f69b209bdbe5036ef509c53b54d0ad3
Jowilson-JowJow <captares@gmail.com>
2025-07-23 20:22:55 -0400

    aula

--- 2°SEMESTRE/UC8-prog_orient_obj/JAVA/aula_java/src/exemplo02.java
+++ 2°SEMESTRE/UC8-prog_orient_obj/JAVA/aula_java/src/exemplo02.java
@@ -1,5 +1,5 @@
 
-public class Main
+public class exemplo02
 {
 	public static void main(String[] args) {
 	 System.out.print("Boa Noite!!! ");
--- 2°SEMESTRE/UC8-prog_orient_obj/JAVA/aula_java/src/exemplo04.java
+++ 2°SEMESTRE/UC8-prog_orient_obj/JAVA/aula_java/src/exemplo04.java
@@ -1,5 +1,6 @@
 
-public class Main
+public class exemplo04
+
 {
 	public static void main(String[] args) {
 	 System.out.print("Boa Noite!!! ");
