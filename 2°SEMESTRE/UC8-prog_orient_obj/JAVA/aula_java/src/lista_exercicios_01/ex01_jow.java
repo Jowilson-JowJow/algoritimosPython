f0a4a02011ddd79460b8d08fa4ce77f691545ca5
Jowilson-JowJow <captares@gmail.com>
2025-07-28 13:17:03 -0400

    resolução 1° lista java

---
+++ 2°SEMESTRE/UC8-prog_orient_obj/JAVA/aula_java/src/lista_exercicios_01/ex01_jow.java
@@ -0,0 +1,27 @@
+//Crie um programa que leia 2 números inteiros e 1 real. Calcule e mostre: o produto do primeiro com a metade do segundo, a soma do triplo do primeiro com o terceiro. O terceiro número digitado ao cubo.
+
+package lista_exercicios_01;
+
+import java.util.Scanner;
+
+public class ex01_jow {
+    public static void main (String [] args){
+        Scanner scan = new Scanner (System.in);
+        System.out.println("Digite um numero inteiro: ");
+        int num1=scan.nextInt();
+        System.out.println("Digite outro numero inteiro: ");
+        int num2=scan.nextInt();
+        System.out.println("Digite outro numero decimal ou inteiro: ");
+        double num3=scan.nextDouble();
+
+        System.out.printf("O 1° numero diditado foi: %d\nO 2° numero digitado foi: %d\nE o 3° numero digitado foi: %.2f",num1,num2,num3);
+
+        double produto=num1*(num2/2);
+        double soma=3*num1+num3;
+        double cubo=Math.pow(num3,3);
+
+        System.out.printf("\nO produto do 1° numero com a metade do 2° numero é %.2f\nO triplo do 1° numero somado com o 3° numero é %.2f\nO cubo do 3° numero é %.2f",produto,soma,cubo);
+
+
+    }
+}
---
+++ 2°SEMESTRE/UC8-prog_orient_obj/JAVA/aula_java/src/lista_exercicios_01/ex02_jow.java
@@ -0,0 +1,25 @@
+//Crie um programa que receba um número inteiro e verifique se ele é maior que 10 se sim, imprima: é maior que 10, senão imprima: é menor que 10.
+
+package lista_exercicios_01;
+
+
+import java.util.Scanner;
+
+public class ex02_jow {
+    public static void main (String[] args){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Digite um numero : ");
+        int num=scan.nextInt();
+
+        if (num>=10){
+            System.out.println("O numero digitdo é MAIOR qu 10.");
+        }
+        else{
+            System.out.println("O numero digitado é MENOR que 10.");
+        }
+
+
+    }
+
+    
+}
