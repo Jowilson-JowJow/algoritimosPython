6d42ae3d34b3f6bf94cdcbdad3be2521ebdb1f08
Jowilson-JowJow <captares@gmail.com>
2025-08-01 21:47:25 -0400

    lista

---
+++ 2°SEMESTRE/UC8-prog_orient_obj/JAVA/aula_java/src/lista_exercicios_01/ex19_jow.java
@@ -0,0 +1,26 @@
+//Crie um algoritmo que calcule a média ponderada das notas de 3 provas. A primeira e a segunda prova têm peso 1 e a terceira tem peso 2. Ao final, mostrar a média do aluno e indicar se o aluno foi aprovado ou reprovado. A nota para aprovação deve ser igual ou superior a 60 pontos.
+package lista_exercicios_01;
+
+import java.util.Scanner;
+
+public class ex19_jow {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Digite a primeira nota: ");
+        Double nota1 = scan.nextDouble();
+        System.out.println("Digite a segunda nota: ");
+        Double nota2 = scan.nextDouble();
+        System.out.println("Digite a terceira nota: ");
+        Double nota3 = scan.nextDouble();
+        Double media = (nota1+nota2+2*nota3)/4;
+        if (media <6){
+            System.out.println("REPROVADO!");
+        }
+        else{
+            System.out.println("APROVADO!");
+        }
+
+
+    }
+    
+}
---
+++ 2°SEMESTRE/UC8-prog_orient_obj/JAVA/aula_java/src/lista_exercicios_01/ex20_jow.java
@@ -0,0 +1,5 @@
+package lista_exercicios_01;
+
+public class ex20_jow {
+    
+}
