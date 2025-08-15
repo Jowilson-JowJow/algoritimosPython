205a013b2da7062789279a42efdb32b8e879ca1b
Jowilson-JowJow <captares@gmail.com>
2025-08-14 21:12:15 -0400

    aula

--- 2°SEMESTRE/UC8-prog_orient_obj/JAVA/aula_java/src/lista_exercicios_02/ex31_jow.java
+++ 2°SEMESTRE/UC8-prog_orient_obj/JAVA/aula_java/src/lista_exercicios_02/ex31_jow.java
@@ -1,7 +1,9 @@
 //Faça um Programa que leia 20 números inteiros e armazene-os em uma LISTA. Armazene os números pares na lista PAR e os números IMPARES na lista ímpar. Imprima os três vetores.
 package lista_exercicios_02;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class ex31_jow {
@@ -9,24 +11,24 @@ public class ex31_jow {
         Scanner scan = new Scanner(System.in);
         
         int[] lista = new int[20];
+        List<Integer> lista_par = new ArrayList<>();
+        List<Integer> lista_impar = new ArrayList<>();
         int i =0;
-        for(i=1;i<20;i++){
-            System.out.printf("Digite o %d° numero: ",i);
+        for(i=0;i<20;i++){
+            System.out.printf("Digite o %d° numero: ",i+1);
             int num = scan.nextInt();
             lista[i]=num;
+            if (num%2==0){
+                lista_par.add(num);
+            }
+            else{
+                lista_impar.add(num);
+            }
+            scan.close();
         }
         System.out.println(Arrays.toString(lista));
-        // int num1 = scan.nextInt();
-        // boolean achou =Arrays.stream(lista).anyMatch(x -> x == num1);
-        // if(achou){
-        //     System.out.println("A lista criada foi: "+Arrays.toString(lista));
-        //     System.out.println("O valor procurado esta na lista!!!");
-        // }
-        // else{
-        //     System.out.println("A lista criada foi: "+Arrays.toString(lista));
-        //     System.out.println("O valor procurado NÃO esta na lista!!!");
-        // }
-        // scan.close();
+        System.out.println(lista_par);
+        System.out.println(lista_impar);
 
     }
 }
---
+++ 2°SEMESTRE/UC8-prog_orient_obj/JAVA/aula_java/src/lista_exercicios_02/ex32_jow.java
@@ -0,0 +1,6 @@
+//Faça um Programa que leia um vetor de 10 caracteres, e diga quantas consoantes foram lidas. Imprima as consoantes.
+package lista_exercicios_02;
+
+public class ex32_jow {
+    
+}
