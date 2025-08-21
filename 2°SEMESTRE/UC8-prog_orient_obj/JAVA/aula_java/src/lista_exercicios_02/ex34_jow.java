b22e538c7cac4a51e23e257c30159b701dca6932
Jowilson-JowJow <captares@gmail.com>
2025-08-20 21:42:00 -0400

    aula

---
+++ 2°SEMESTRE/UC8-prog_orient_obj/JAVA/aula_java/src/lista_exercicios_02/ex34_jow.java
@@ -0,0 +1,45 @@
+//Escreva um programa que receba um número inteiro maior do que 1, e verifique se o número fornecido é primo ou não.
+package lista_exercicios_02;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class ex34_jow {
+    public static void main( String[] args){
+        Scanner scan = new Scanner(System.in);
+        List<Integer> num_primos=new ArrayList<>();
+        System.out.println("Digite um numero maior que 1: ");
+        int num = scan.nextInt();
+        int i;
+        //int j;
+
+        if(num<=1){
+            System.out.println("Numero Errado!!!");
+        }
+        if (num>1){
+            for(i=1; i<=num; i++){                
+                if(num%i==0 ){
+                    num_primos.add(i);
+                }
+            }
+        }
+        if (num_primos.size()==2){
+            System.out.println("O numero digitado é primo!!!");
+        }
+        else{
+            System.out.println("O numero digitado não é primo!!!");
+        }
+
+        // System.out.println("Minha lista tem "+num_primos.size());
+
+        // for(j=0;j<num_primos.size();j++){
+        //     System.out.println(num_primos.get(j));
+        // }
+
+        scan.close();
+    
+    }
+}
+
+
---
+++ 2°SEMESTRE/UC8-prog_orient_obj/JAVA/aula_java/src/lista_exercicios_02/ex35_jow.java
@@ -0,0 +1,26 @@
+//Escreva um programa que leia um inteiro não negativo n e imprima a soma dos n primeiros números primos.
+package lista_exercicios_02;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class ex35_jow {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Digite um numero inteiro:");
+        int num = scan.nextInt();
+        List<Integer> num_digtado = new ArrayList<>();
+        for (int i=0; i<num+1; i++){
+            num_digtado.add(i);
+        }
+        int soma = 0;
+        for(int j=0; j<num_digtado.size(); j++){
+            soma +=num_digtado.get(j);
+        }
+        System.out.println(soma);
+        scan.close();
+        
+    }
+    
+}
