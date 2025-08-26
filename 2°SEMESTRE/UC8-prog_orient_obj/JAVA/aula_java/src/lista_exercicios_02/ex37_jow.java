b8341cf0763218abafc5e213ff5dd877ea900b0b
Jowilson-JowJow <captares@gmail.com>
2025-08-25 21:34:33 -0400

    Create ex37_jow.java

---
+++ 2°SEMESTRE/UC8-prog_orient_obj/JAVA/aula_java/src/lista_exercicios_02/ex37_jow.java
@@ -0,0 +1,6 @@
+//Crie um programa que calcula a associação em paralelo de dois resistores R1 e R2 fornecidos pelo usuário via teclado. O programa fica pedindo estes valores e calculando até que o usuário entre com um valor para resistência igual a zero.
+package lista_exercicios_02;
+
+public class ex37_jow {
+    
+}
