dcc68f1b2d396e475f5b66f7d26e9c133d9ed8b5
Jowilson-JowJow <captares@gmail.com>
2025-08-29 18:10:58 -0400

    aula

---
+++ 2°SEMESTRE/UC8-prog_orient_obj/JAVA/aula_java/src/lista_exercicios_02/ex18Jow.java
@@ -0,0 +1,6 @@
+//Escreva um algoritmo que leia um número inteiro entre 100 e 9999 e imprima na saída cada um dos algarismos que compõem o número.
+package lista_exercicios_02;
+
+public class ex18Jow {
+    
+}
---
+++ 2°SEMESTRE/UC8-prog_orient_obj/JAVA/aula_java/src/lista_exercicios_02/ex39Jow.java
@@ -0,0 +1,35 @@
+//Faça um programa que gera um número aleatório de 1 a 100. O usuário deve tentar acertar qual o número foi gerado, a cada tentativa o programa deverá informar se o chute e menor ou maior que o número gerado. O programa acaba quando o usuário acerta o número gerado. O programa deve informar em quantas tentativas o número foi descoberto.
+package lista_exercicios_02;
+//bibliotecas importadas 
+import java.util.Scanner;
+import java.util.Random;
+
+public class ex39Jow {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        //pra gerar um numero randomigo e salvar em uma variavel
+        Random rate = new Random();
+        int numero = rate.nextInt(101);
+        System.out.println("Vamos brincar?\nEu acabei de escolher um numero entre 0 e 100, VOCÊ consegue descobrir qual foi esse numero escolhido?");
+        //loop para tentar descobrir o numero escolhido, o loop encerra quando o numero digitado for igual ao  numero escolhido
+        System.out.println("Tente adivinhar, Qual foi o numero escolhido? ");
+        int num_digitado=scan.nextInt();
+        while(true){
+            if(num_digitado==numero){
+                System.out.println("PARABÉNS!!!\nVocê acertou o numero!");
+                break;
+            }
+            else{
+                if(num_digitado<numero){
+                    System.out.println("O numero digitado é menor que o numero escolhido: \nTente de novo: ");
+                    num_digitado=scan.nextInt();
+                }
+                else if(num_digitado>numero){
+                    System.out.println("O numero digitado é maior que o numero escolhido:\nTente de novo: ");
+                    num_digitado=scan.nextInt();
+                }
+            }
+        }
+        scan.close();
+    }    
+}
