8217f263e6f15cc856c7b8e9c8c4270d4c6ceb64
Jowilson-JowJow <captares@gmail.com>
2025-10-01 19:06:29 -0400

    aula

---
+++ 2°SEMESTRE/UC8-prog_orient_obj/JAVA/aula_java/CLASSES/ExerciciosClassePython/ex1Jow.py
@@ -0,0 +1,23 @@
+
+class Pessoa:
+    def __init__(self, nome, idade, endereco):
+        self.nome = nome
+        self.idade = idade
+        self.endereco = endereco
+
+    def mostrar_nome(self):
+        print(f"Seu nome é: {self.nome}")
+
+    def alterar_idade(self, nova_idade):
+        self.idade = nova_idade
+
+    def mostrar_endereco(self):
+        print(f"O seu endereço é: {self.endereco}")
+
+
+p1 = Pessoa("jowjow", 48, "rua francisco mario, 108")
+p1.mostrar_nome()
+print(f"Sua idade é: {p1.idade} anos")
+p1.alterar_idade(22)
+print(f"Idade corrigida: {p1.idade} anos")
+p1.mostrar_endereco()
