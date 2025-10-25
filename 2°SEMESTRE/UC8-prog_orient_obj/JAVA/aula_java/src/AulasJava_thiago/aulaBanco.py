9ddb8e360e922d7232844f4258e1733b5431b915
Jowilson-JowJow <captares@gmail.com>
2025-10-25 09:11:24 -0400

    aula

--- 2°SEMESTRE/UC8-prog_orient_obj/JAVA/aula_java/src/AulasJava_thiago/aulaBanco.py
+++ 2°SEMESTRE/UC8-prog_orient_obj/JAVA/aula_java/src/AulasJava_thiago/aulaBanco.py
@@ -1,2 +1,21 @@
-import mysql.conector
+import mysql.connector
+
+class Database:
+    def __init__ (self, banco = "perkal") -> None:
+        self.banco = banco
+        self.host = "localhost"
+        self.user = "root"
+    
+    def connect(self):
+        self.conn = mysql.connector.connect(host=self.host, database=self.banco, user = self.user, password ="")
+        if self.conn.is_connected():
+            self.cursor = self.conn.cursor()
+            db_info = self.conn.get_server_info()
+            print("conectado com sucesso")
+        else:
+            print("erro")
+
+db = Database()
+db.connect()
+
 
