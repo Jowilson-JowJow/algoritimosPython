b653dcde91a2f28cd1718608437e561d4d64227e
Jowilson-JowJow <captares@gmail.com>
2025-10-28 20:08:19 -0400

    estudo

--- 2°SEMESTRE/Aulas_Calebe/ex1Jow.py
+++ 2°SEMESTRE/Aulas_Calebe/ex1Jow.py
@@ -1,2 +1,84 @@
 #Exercício 1 - Sistema de Agendamento (Clínica de Estética)
 
+class Cliente:
+    def __init__ (self, nome, cpf, telefone, email, endereco):
+        self.nome = nome 
+        self.cpf = cpf
+        self.telefone = telefone
+        self.email = email
+        self.endereco = endereco
+
+    def cadastrar(self):
+        print(f"Cliente: {self.nome} | CPF: {self.cpf}\nCadastrado com Sucesso!!!")
+
+    def atualizar(self, nome, email , telefone, endereco):
+        self.nome = nome
+        self.email = email
+        self.telefone = telefone
+        self.endereco = endereco
+
+    def ver_agendamento(self):
+
+
+class Servico:
+    def __init__(self, codigo, nome, descricao, duracao, preco):
+        self.codigo = codigo
+        self.nome = nome
+        self.descricao = descricao
+        self.duracao = duracao
+        self.preco = preco
+
+    def calcular_preco(self):
+
+    def atualisar_descricao(self):
+
+    def lista_servico(self):
+
+
+class Profissional:
+    def __init__(self, nome, especialidade, registro_profissional, telefone, disponivel):
+        self.nome = nome
+        self.especialidade = especialidade
+        self.registro_profissional = registro_profissional
+        self.telefone = telefone
+        self.disponivel = disponivel
+
+    def ver_disponibilidade(self):
+
+    def add_horario(self):
+
+
+    def remove_horario(self):
+
+
+class Agendamento:
+    def __init__(self, codigo, data, hora, status):
+        self.codigo = codigo
+        self.data = data
+        self.hora = hora
+        self.status = status
+
+    def criar_agendamento(self):
+
+
+    def cancelar_reserva(self):
+
+
+    def comfirmar_reserva(self):
+
+
+class Pagamento:
+    def __init__(self, codigo, valor, forma_pagamento, data, status):
+        self.codigo = codigo
+        self.valor = valor
+        self.forma_pagamento = forma_pagamento
+        self.data = data
+        self.status = status
+
+    def processar_pagamento(self):
+
+    def ver_status(self):
+
+    def emitir_recibo(self):
+        
+
--- 2°SEMESTRE/Aulas_Calebe/ex2Jow.py
+++ 2°SEMESTRE/Aulas_Calebe/ex2Jow.py
@@ -36,6 +36,53 @@ class Produto:
     def info(self):
         print(f"Nome do produto: {self.nome} | Preço: {self.preco:.2f} | Quantidade: {self.qtd}")
 
+
+class Pedido:
+    def __init__(self,codigo, data, cliente,associado, valor_total, status):
+        self.codigo = codigo
+        self.data = data
+        self.cliente = cliente
+        self.associado = associado
+        self.valor_total = valor_total
+        self.status = status
+
+    def adicionar_produtos():
+
+    def remover_produtos():
+
+
+    def valor_compra():
+
+class Item_pedido:
+    def __init__(self, codigo, qtd, preco_unitario, sub_total):
+        self.codigo = codigo
+        self.qtd = qtd
+        self.preco_unitario = preco_unitario
+        self.sub_total = sub_total
+
+    def calcular_subtotal(self):
+
+    def atualisar_qtd(self):
+
+    def exibir_info (self):
+
+
+class Pagamento:
+    def __init__(self, codigo, valor_pago, forma_pagamento, data):
+        self.codigo = codigo
+        self.valor_pago = valor_pago
+        self.forma_pagamento = forma_pagamento
+        self.data = data
+
+    def processar_pagamento(self):
+
+    def confirmar_pagamento(self):
+
+    def emitir_recibo(self):
+        
+
+
+
 lista_clientes = []
 cliente1 = Clientes(1, "jowjow","000000000000","aaaa@gmail.com", "67998745847" )
 
